9590bd47849dcf3855f7e9d851f2270991a5b924
Artyom Kisurin <48124997+artykisa@users.noreply.github.com>
2020-07-16 15:28:07 +0300

    Update settings.py

--- djangolab/settings.py
+++ djangolab/settings.py
@@ -128,7 +128,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'vagabond228@gmail.com'
-EMAIL_HOST_PASSWORD = 'Megusta5ko'
+EMAIL_HOST_PASSWORD = '*'
 
 LOGGING_CONFIG = None
 LOGGING = {
