1d22f6cc17045105ef8cf34014550109acb79c7b
artykisa <crazypluton@gmail.com>
2020-07-16 15:08:33 +0300

    Init

--- djangolab/settings.py
+++ djangolab/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'djangolab.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres2',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
